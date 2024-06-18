e477b7f49a9d2bc83d1c86840bbca12223668088
Sonnengott <80594111+Silas217209@users.noreply.github.com>
2024-06-18 22:44:33 +0200

    playing moves (wip)

--- source/main.cpp
+++ source/main.cpp
@@ -14,9 +14,14 @@ const uint64_t column_masks[7] = {
     0b0000000000000000000000'0000010'0000010'0000010'0000010'0000010'0000010,
     0b0000000000000000000000'0000001'0000001'0000001'0000001'0000001'0000001,
 };
-
-const unsigned int row_count = 6;
-const unsigned int col_count = 7;
+const uint64_t row_masks[6] = {
+    0b0000000000000000000000'1111111'0000000'0000000'0000000'0000000'0000000,
+    0b0000000000000000000000'0000000'1111111'0000000'0000000'0000000'0000000,
+    0b0000000000000000000000'0000000'0000000'1111111'0000000'0000000'0000000,
+    0b0000000000000000000000'0000000'0000000'0000000'1111111'0000000'0000000,
+    0b0000000000000000000000'0000000'0000000'0000000'0000000'1111111'0000000,
+    0b0000000000000000000000'0000000'0000000'0000000'0000000'0000000'1111111,
+};
 
 enum Turn { yellow, red };
 
@@ -35,13 +40,29 @@ class Game {
 
 class Board {
   public:
-    uint64_t yellow_bitbaord = 0b0;
-    uint64_t red_bitbaord = 0b0;
+    uint64_t yellow_bitbaord = 0;
+    uint64_t red_bitbaord = 0b10;
 
-    void show_board() const {
-        for (int row = (row_count - 1); row >= 0; row--) {
-            for (int col = col_count; col >= 0; col--) {
-                uint64_t index = static_cast<uint64_t>(row) * col_count + col;
+    auto play_move(unsigned int col, Turn turn) {
+        // row out of range 0 <= col <= 6
+        if (col > 6) {
+            return 1;
+        }
+
+        uint64_t current_board;
+        switch (turn) {
+            case Turn::red:
+                current_board = red_bitbaord;
+            case Turn::yellow:
+                current_board = yellow_bitbaord;
+        }
+
+    }
+
+    void show_board() {
+        for (int row = 5; row >= 0; row--) {
+            for (int col = 6; col >= 0; col--) {
+                uint64_t index = static_cast<uint64_t>(row) * 7 + col;
                 uint64_t field_mask = static_cast<uint64_t>(1) << index;
 
                 if ((yellow_bitbaord & field_mask) != 0) {
@@ -57,9 +78,9 @@ class Board {
     }
 
     static void show_any_board(uint64_t board) {
-        for (int row = (row_count - 1); row >= 0; row--) {
-            for (int col = col_count; col >= 0; col--) {
-                uint64_t index = static_cast<uint64_t>(row) * col_count + col;
+        for (int row = 5; row >= 0; row--) {
+            for (int col = 6; col >= 0; col--) {
+                uint64_t index = static_cast<uint64_t>(row) * 7 + col;
                 uint64_t field_mask = static_cast<uint64_t>(1) << index;
 
                 if ((board & field_mask) != 0) {
@@ -74,10 +95,10 @@ class Board {
 };
 
 auto main() -> int {
-    for (uint64_t board : column_masks) {
+    for (uint64_t board : row_masks) {
         Board::show_any_board(board);
+        std::cout << "\n";
     }
 
     return 0;
 }
-
